66bf121e7363b19f168510ae56e109d0da045df6
AshwinSelvaraj <ashwinselvaraj26@gmail.com>
2026-05-20 13:07:44 +0530

    Fix import statement and update timer engine reference in main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import RoutineBuilder from './routineBuilder.js';
-import TimerEngine from './timerEngine.js';
+import TimeEngine from './timeEngine.js';
 
 document.getElementById('start-routine-btn').addEventListener('click', () => {
     
     const standardQueue = RoutineBuilder.getFinalQueue();
     
     if (standardQueue) {
-        TimerEngine.startEngine(standardQueue);
+        TimeEngine.startEngine(standardQueue);
     }
 });
 
