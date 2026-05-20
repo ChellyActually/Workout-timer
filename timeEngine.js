1d6bd426d7c0dcf6b5291bcefd1af28d5b4cf1a2
geethasree100108 <geethasree100108@gmail.com>
2026-05-20 13:01:32 +0530

    changed..

---
+++ timeEngine.js
@@ -0,0 +1,48 @@
+const TimeEngine = {
+    currentQueue: [],
+    currentIndex:0,
+    currentRemainingTime: 0,
+    timerId: null
+
+ startEngine (validatedQueue){
+    if(!validatedQueue || validatedQueue.length===0){
+        alert("Workout queue is empty!!");
+        return;
+    }
+    this.currentQueue= validatedQueue;
+    this.currentIndex = 0;
+    this.loadCurrentInterval();
+},
+loadCurrentInterval(){
+    const currentItem=this.currentQueue[this.currentIndex];
+    this.currentRemainingTime=currentItem.duration;
+    TimePresenter.onIntervalChanged(currentItem);
+    this.runClockLoop();
+},
+runClockLoop(){
+    this.timerId=setInterval(()=>{
+       this.currentRemainingTime--;
+       TimerPresenter.onTick(this.currentRemainingTime);
+       if(this.currentRemainingTime<=0){
+        this.stopClockLoop();
+        this.nextInterval();
+       }
+   },1000);
+},
+stopClockLoop(){
+ if(this.timerId){
+    clearInterval(this.timerId);
+ }
+
+},
+nextInterval(){
+    this.currentIndex++;
+    if(this.currentIndex < this.currentQueue.length){
+        this.loadcurrentInterval();
+    }
+    else{
+        TimerPresenter.onWorkoutCompleted();
+    }
+
+},
+};
